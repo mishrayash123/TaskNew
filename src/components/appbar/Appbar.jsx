38fe18bd1a052eb232c771e776d6613e26011755
yash kumar mishra <mishrayash3778@gmail.com>
2023-07-14 11:21:29 +0530

    Internship Final Round Task Completed

--- src/components/appbar/Appbar.jsx
+++ src/components/appbar/Appbar.jsx
@@ -8,19 +8,131 @@ import {
   TopIconBadge,
   TopAvatar
 } from "./AppbarStyles";
+import React, { useState } from 'react';
+import Modal1 from 'react-modal';
+import Modal2 from 'react-modal';
+import Modal3 from 'react-modal';
 
 import { NotificationsNone, Language, Settings } from "@material-ui/icons";
 
 function Appbar() {
+  const [isOpen1, setIsOpen1] = useState(false);
+  const [isOpen2, setIsOpen2] = useState(false);
+  const [isOpen3, setIsOpen3] = useState(false);
+
+  const openModal1 = () => {
+    setIsOpen1(true);
+  };
+
+  const closeModal1 = () => {
+    setIsOpen1(false);
+  };
+
+  
+  const openModal2 = () => {
+    setIsOpen2(true);
+  };
+
+  const closeModal2 = () => {
+    setIsOpen2(false);
+  };
+  
+  const openModal3 = () => {
+    setIsOpen3(true);
+  };
+
+  const closeModal3 = () => {
+    setIsOpen3(false);
+  };
+
+
   return (
     <Container>
+      <Modal1
+        isOpen={isOpen1}
+        onRequestClose={closeModal1}
+        contentLabel="Popup Message"
+        style={{
+          overlay: {
+            backgroundColor: 'rgba(0, 0, 0, 0.5)'
+          },
+          content: {
+            width: '300px',
+            height: '200px',
+            margin: 'auto',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: 'pink',
+            borderRadius: '25%',
+            boxShadow: '0 2px 4px rgba(0, 0, 0, 0.3)',
+            color:"blue"
+          }
+        }}
+      >
+        <p>Notifications</p>
+        <button style={{backgroundColor:"black",color:"white",padding:10,borderRadius:'25%',marginTop:5}} onClick={closeModal1}> OK </button>
+      </Modal1>
+      <Modal2
+        isOpen={isOpen2}
+        onRequestClose={closeModal2}
+        contentLabel="Popup Message"
+        style={{
+          overlay: {
+            backgroundColor: 'rgba(0, 0, 0, 0.5)'
+          },
+          content: {
+            width: '300px',
+            height: '200px',
+            margin: 'auto',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: 'pink',
+            borderRadius: '25%',
+            boxShadow: '0 2px 4px rgba(0, 0, 0, 0.3)',
+            color:"blue"
+          }
+        }}
+      >
+        <p>Settings</p>
+        <button style={{backgroundColor:"black",color:"white",padding:10,borderRadius:'25%',marginTop:5}} onClick={closeModal2}> OK </button>
+      </Modal2>
+      <Modal3
+        isOpen={isOpen3}
+        onRequestClose={closeModal3}
+        contentLabel="Popup Message"
+        style={{
+          overlay: {
+            backgroundColor: 'rgba(0, 0, 0, 0.5)'
+          },
+          content: {
+            width: '300px',
+            height: '200px',
+            margin: 'auto',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: 'pink',
+            borderRadius: '25%',
+            boxShadow: '0 2px 4px rgba(0, 0, 0, 0.3)',
+            color:"blue"
+          }
+        }}
+      >
+        <p>Admin Profile's Detailes</p>
+        <button style={{backgroundColor:"black",color:"white",padding:10,borderRadius:'25%',marginTop:5}} onClick={closeModal3}> OK </button>
+      </Modal3>
       <Wrapper>
         <TopLeft>
           <Logo>Admin Dev</Logo>
         </TopLeft>
         <TopRight>
           <IconContainer>
-            <NotificationsNone />
+            <NotificationsNone onMouseEnter={openModal1}/>
             <TopIconBadge>2</TopIconBadge>
           </IconContainer>
           <IconContainer>
@@ -28,9 +140,9 @@ function Appbar() {
             <TopIconBadge>2</TopIconBadge>
           </IconContainer>
           <IconContainer>
-            <Settings />
+            <Settings onMouseEnter={openModal2} />
           </IconContainer>
-          <TopAvatar src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZHoqg4ikD0Gp1l_yc2jpvEdN8Xddy03FZgQ&usqp=CAU" />
+          <TopAvatar onMouseEnter={openModal3} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZHoqg4ikD0Gp1l_yc2jpvEdN8Xddy03FZgQ&usqp=CAU" />
         </TopRight>
       </Wrapper>
     </Container>
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -6,6 +6,15 @@ import {
   List,
   ListItem
 } from "./SidebarStyles";
+import React, { useState } from 'react';
+import Modal1 from 'react-modal';
+import Modal2 from 'react-modal';
+import Modal3 from 'react-modal';
+import Modal4 from 'react-modal';
+import Modal5 from 'react-modal';
+import Modal6 from 'react-modal';
+import Modal7 from 'react-modal';
+import Modal8 from 'react-modal';
 
 import {
   LineStyle,
@@ -24,8 +33,294 @@ import {
 import { Link } from "react-router-dom";
 
 function Sidebar() {
+
+  const [isOpen1, setIsOpen1] = useState(false);
+  const [isOpen2, setIsOpen2] = useState(false);
+  const [isOpen3, setIsOpen3] = useState(false);
+  const [isOpen4, setIsOpen4] = useState(false);
+  const [isOpen5, setIsOpen5] = useState(false);
+  const [isOpen6, setIsOpen6] = useState(false);
+  const [isOpen7, setIsOpen7] = useState(false);
+  const [isOpen8, setIsOpen8] = useState(false);
+ 
+
+  const openModal1 = () => {
+    setIsOpen1(true);
+  };
+
+  const closeModal1 = () => {
+    setIsOpen1(false);
+  };
+
+  
+  const openModal2 = () => {
+    setIsOpen2(true);
+  };
+
+  const closeModal2 = () => {
+    setIsOpen2(false);
+  };
+  
+  const openModal3 = () => {
+    setIsOpen3(true);
+  };
+
+  const closeModal3 = () => {
+    setIsOpen3(false);
+  };
+  
+  const openModal4 = () => {
+    setIsOpen4(true);
+  };
+
+  const closeModal4 = () => {
+    setIsOpen4(false);
+  };
+  
+  const openModal5 = () => {
+    setIsOpen5(true);
+  };
+
+  const closeModal5 = () => {
+    setIsOpen5(false);
+  };
+  
+  const openModal6 = () => {
+    setIsOpen6(true);
+  };
+
+  const closeModal6 = () => {
+    setIsOpen6(false);
+  };
+  
+  const openModal7 = () => {
+    setIsOpen7(true);
+  };
+
+  const closeModal7 = () => {
+    setIsOpen7(false);
+  };
+  const openModal8 = () => {
+    setIsOpen8(true);
+  };
+
+  const closeModal8 = () => {
+    setIsOpen8(false);
+  };
+
   return (
     <Container>
+       <Modal1
+        isOpen={isOpen1}
+        onRequestClose={closeModal1}
+        contentLabel="Popup Message"
+        style={{
+          overlay: {
+            backgroundColor: 'rgba(0, 0, 0, 0.5)'
+          },
+          content: {
+            width: '300px',
+            height: '200px',
+            margin: 'auto',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: 'pink',
+            borderRadius: '25%',
+            boxShadow: '0 2px 4px rgba(0, 0, 0, 0.3)',
+            color:"blue"
+          }
+        }}
+      >
+        <p>Home page of Admin pannel</p>
+        <button style={{backgroundColor:"black",color:"white",padding:10,borderRadius:'25%',marginTop:5}} onClick={closeModal1}> OK </button>
+      </Modal1>
+      <Modal2
+        isOpen={isOpen2}
+        onRequestClose={closeModal2}
+        contentLabel="Popup Message"
+        style={{
+          overlay: {
+            backgroundColor: 'rgba(0, 0, 0, 0.5)'
+          },
+          content: {
+            width: '300px',
+            height: '200px',
+            margin: 'auto',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: 'pink',
+            borderRadius: '25%',
+            boxShadow: '0 2px 4px rgba(0, 0, 0, 0.3)',
+            color:"blue"
+          }
+        }}
+      >
+        <p>Analytics report : Total active users / month</p>
+        <p>| Total sales | Revenue | Cost</p>
+        <button style={{backgroundColor:"black",color:"white",padding:10,borderRadius:'25%',marginTop:5}} onClick={closeModal2}> OK </button>
+      </Modal2>
+      <Modal3
+        isOpen={isOpen3}
+        onRequestClose={closeModal3}
+        contentLabel="Popup Message"
+        style={{
+          overlay: {
+            backgroundColor: 'rgba(0, 0, 0, 0.5)'
+          },
+          content: {
+            width: '300px',
+            height: '200px',
+            margin: 'auto',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: 'pink',
+            borderRadius: '25%',
+            boxShadow: '0 2px 4px rgba(0, 0, 0, 0.3)',
+            color:"blue"
+          }
+        }}
+      >
+        <p>User's Data/List</p>
+        <button style={{backgroundColor:"black",color:"white",padding:10,borderRadius:'25%',marginTop:5}} onClick={closeModal3}> OK </button>
+      </Modal3>
+      <Modal4
+        isOpen={isOpen4}
+        onRequestClose={closeModal4}
+        contentLabel="Popup Message"
+        style={{
+          overlay: {
+            backgroundColor: 'rgba(0, 0, 0, 0.5)'
+          },
+          content: {
+            width: '300px',
+            height: '200px',
+            margin: 'auto',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: 'pink',
+            borderRadius: '25%',
+            boxShadow: '0 2px 4px rgba(0, 0, 0, 0.3)',
+            color:"blue"
+          }
+        }}
+      >
+        <p>About Us Page</p>
+        <button style={{backgroundColor:"black",color:"white",padding:10,borderRadius:'25%',marginTop:5}} onClick={closeModal4}> OK </button>
+      </Modal4>
+      <Modal5
+        isOpen={isOpen5}
+        onRequestClose={closeModal5}
+        contentLabel="Popup Message"
+        style={{
+          overlay: {
+            backgroundColor: 'rgba(0, 0, 0, 0.5)'
+          },
+          content: {
+            width: '300px',
+            height: '200px',
+            margin: 'auto',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: 'pink',
+            borderRadius: '25%',
+            boxShadow: '0 2px 4px rgba(0, 0, 0, 0.3)',
+            color:"blue"
+          }
+        }}
+      >
+        <p>Our Product's List</p>
+        <button style={{backgroundColor:"black",color:"white",padding:10,borderRadius:'25%',marginTop:5}} onClick={closeModal5}> OK </button>
+      </Modal5>
+      <Modal6
+        isOpen={isOpen6}
+        onRequestClose={closeModal6}
+        contentLabel="Popup Message"
+        style={{
+          overlay: {
+            backgroundColor: 'rgba(0, 0, 0, 0.5)'
+          },
+          content: {
+            width: '300px',
+            height: '200px',
+            margin: 'auto',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: 'pink',
+            borderRadius: '25%',
+            boxShadow: '0 2px 4px rgba(0, 0, 0, 0.3)',
+            color:"blue"
+          }
+        }}
+      >
+        <p>Transactions List</p>
+        <button style={{backgroundColor:"black",color:"white",padding:10,borderRadius:'25%',marginTop:5}} onClick={closeModal6}> OK </button>
+      </Modal6>
+      <Modal7
+        isOpen={isOpen7}
+        onRequestClose={closeModal7}
+        contentLabel="Popup Message"
+        style={{
+          overlay: {
+            backgroundColor: 'rgba(0, 0, 0, 0.5)'
+          },
+          content: {
+            width: '300px',
+            height: '200px',
+            margin: 'auto',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: 'pink',
+            borderRadius: '25%',
+            boxShadow: '0 2px 4px rgba(0, 0, 0, 0.3)',
+            color:"blue"
+          }
+        }}
+      >
+        <p>All Reports List</p>
+        <button style={{backgroundColor:"black",color:"white",padding:10,borderRadius:'25%',marginTop:5}} onClick={closeModal7}> OK </button>
+      </Modal7>
+      <Modal8
+        isOpen={isOpen8}
+        onRequestClose={closeModal8}
+        contentLabel="Popup Message"
+        style={{
+          overlay: {
+            backgroundColor: 'rgba(0, 0, 0, 0.5)'
+          },
+          content: {
+            width: '300px',
+            height: '200px',
+            margin: 'auto',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: 'pink',
+            borderRadius: '25%',
+            boxShadow: '0 2px 4px rgba(0, 0, 0, 0.3)',
+            color:"blue"
+          }
+        }}
+      >
+        <p>List of all notifications</p>
+        <p>Mail | Feedback | Messages</p>
+        <p>Management</p>
+        <button style={{backgroundColor:"black",color:"white",padding:10,borderRadius:'25%',marginTop:5}} onClick={closeModal8}> OK </button>
+      </Modal8>
       <Wrapper>
         <SidebarMenu>
           <Title>Dashboard</Title>
@@ -33,21 +328,21 @@ function Sidebar() {
             <Link to="/" className="link">
               <ListItem>
                 <LineStyle className="icon" />
-                <div>Home</div>
+                <div onMouseEnter={openModal1}>Home</div>
               </ListItem>
             </Link>
             <ListItem>
               <Timeline className="icon" />
-              <div>Analytics</div>
+              <div onMouseEnter={openModal2}>Analytics</div>
             </ListItem>
             <ListItem>
               <TrendingUp className="icon" />
-              <div>Sales</div>
+              <div onMouseEnter={openModal2}>Sales</div>
             </ListItem>
             <Link to="/users" className="link">
               <ListItem>
                 <Group className="icon" />
-                <div>Users</div>
+                <div onMouseEnter={openModal3}>Users</div>
               </ListItem>
             </Link>
           </List>
@@ -55,24 +350,24 @@ function Sidebar() {
           <List>
             <ListItem>
               <Info className="icon" />
-              <div>About</div>
+              <div onMouseEnter={openModal4}>About</div>
             </ListItem>
             <Link to="/products" className="link">
               <ListItem>
                 <Storefront className="icon" />
-                <div>Products</div>
+                <div onMouseEnter={openModal5}>Products</div>
               </ListItem>
             </Link>
             <ListItem>
               <AttachMoney className="icon" />
-              <div>Transactions</div>
+              <div onMouseEnter={openModal6}>Transactions</div>
             </ListItem>
             <ListItem>
               <BarChart className="icon" />
-              <div>Reports</div>
+              <div onMouseEnter={openModal7}>Reports</div>
             </ListItem>
           </List>
-          <Title>Notifications</Title>
+          <Title onMouseEnter={openModal8}>Notifications</Title>
           <List>
             <ListItem>
               <MailOutline className="icon" />
